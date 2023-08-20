b2ce3ebf557c79ff1bbd1aad04591690d2e824b3
yewonniy <ae6652@naver.com>
2023-08-21 00:17:14 +0900

    0821

--- bj/0820/9095.py
+++ bj/0820/9095.py
@@ -2,21 +2,26 @@ import sys
 t = int(input())
 
 
+def fact(n):
+    result = 1
+    if n == 0:
+        return 1
+    else:
+        for i in range(n, 1, -1):
+            result = result * i
+    return result
+
+
 def count(n):
     result = 0
-    memo = set()
     for i in range(0, n // 3 + 1):
         for j in range(0, n // 2 + 1):
             for k in range(0, n+1):
                 if 3*i + 2*j + 1*k == n:
-                    memo.add((3, i, 2, j, 1, k))
-                    memo.add((3, i, 1, k, 2, j))
-                    memo.add((2,j, 1, k, 3, i))
-                    memo.add((2, j, 3, i, 1, k))
-                    memo.add((1, k, 2, j, 3, i))
-                    memo.add((1, k, 3, i, 2, j))
-    print(memo)
-    result = len(memo)
+                    if (i == 0 and j == 0) or (i == 0 and k == 0) or (j ==0 and k == 0):
+                        result += 1
+                    else:
+                        result += (fact(i+j+k)//(fact(i)*fact(k)*fact(j)))
     return result
 
 
