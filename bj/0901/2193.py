71f55207f5b07087152a236d292e231f9e4ffb54
yewonniy <ae6652@naver.com>
2023-09-01 23:57:10 +0900

    0901

---
+++ bj/0901/2193.py
@@ -0,0 +1,50 @@
+import sys
+sys.setrecursionlimit(10000)
+N = int(input())
+cnt = 0
+
+
+def pin(n):
+    global cnt
+    if len(str(n)) == N:
+        cnt += 1
+        return cnt
+    if n % 10 == 1:
+        n = n*10
+        pin(n)
+    else:
+        n *= 10
+        pin(n)
+        n += 1
+        pin(n)
+
+
+if N == 1:
+    print(1)
+else:
+    pin(1)
+    print(cnt)
+
+# 위에꺼 시간 초과 나서 아래 꺼로
+# 아래껀 메모리 초과..
+M = int(input())
+
+
+def pinary_(n):
+    result = [1, 0]
+    for i in range(2, N-1):
+        cnt_zero = result.count(0)
+        cnt_one = result.count(1)
+        result = []
+        for k in range(cnt_zero):
+            result.append(0)
+            result.append(1)
+        for j in range(cnt_one):
+            result.append(0)
+    print(len(result))
+
+
+if M == 1 or M == 2:
+    print(1)
+else:
+    pinary_(M)
