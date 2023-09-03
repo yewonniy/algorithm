f0549d0b25a8f88a21e8c50646f8fa15a8f20132
yewonniy <ae6652@naver.com>
2023-09-04 00:44:17 +0900

    0904

--- bj/0904/4963.py
+++ bj/0904/4963.py
@@ -1,5 +1,62 @@
 import sys
+sys.setrecursionlimit(10000)
+
 m, n = map(int, input().split())
+
+
+def direction(array, i, j, cnt, M, N, visited):
+    ni = i
+    nj = j
+    if i - 1 >= 0:
+        if array[i - 1][j] == 1 and (i - 1, j) not in visited:
+            visited.append((i - 1, j))
+            if i-1 > 0:
+                ni = i-1
+                direction(array, ni, nj, cnt, M, N, visited)
+        if j - 1 >= 0:
+            if array[i - 1][j - 1] == 1 and (i - 1, j - 1) not in visited:
+                visited.append((i - 1, j - 1))
+                if i - 1 > 0 or j - 1> 0:
+                    ni = i-1
+                    nj = j-1
+                    direction(array, ni, nj, cnt, M, N, visited)
+        if j + 1 < M:
+            if array[i - 1][j + 1] == 1 and (i - 1, j + 1) not in visited:
+                visited.append((i - 1, j + 1))
+                ni = i-1
+                nj = j+1
+                direction(array, ni, nj, cnt, M, N, visited)
+    if i + 1 < N:
+        if array[i + 1][j] == 1 and (i + 1, j) not in visited:
+            visited.append((i + 1, j))
+            ni = i+1
+            nj = j
+            direction(array, ni, nj, cnt, M, N, visited)
+        if j - 1 >= 0:
+            if array[i + 1][j - 1] == 1 and (i + 1, j - 1) not in visited:
+                visited.append((i + 1, j - 1))
+                ni = i+1
+                nj = j-1
+                direction(array, ni, nj, cnt, M, N, visited)
+        if j + 1 < M:
+            if array[i + 1][j + 1] == 1 and (i + 1, j + 1) not in visited:
+                visited.append((i + 1, j + 1))
+                ni = i+1
+                nj = j+1
+                direction(array, ni, nj, cnt, M, N, visited)
+    if j - 1 >= 0:
+        if array[i][j - 1] == 1 and (i, j - 1) not in visited:
+            visited.append((i, j - 1))
+            ni = i
+            nj = j-1
+            direction(array, ni, nj, cnt, M, N, visited)
+    if j + 1 < M:
+        if array[i][j + 1] == 1 and (i, j + 1) not in visited:
+            visited.append((i, j + 1))
+            ni = i
+            nj = j+1
+            direction(array, ni, nj, cnt, M, N, visited)
+    return 1
 
 
 def count(array, M, N):
@@ -10,44 +67,7 @@ def count(array, M, N):
         for j in range(len(array[i])):
             if array[i][j] == 1 and (i, j) not in visited:
                 visited.append((i, j))
-                print((i, j))
-                ni = i
-                nj = j
-                if i-1 >= 0:
-                    if array[i-1][j] == 1 and (i-1, j) not in visited:
-                        visited.append((i-1, j))
-                        memo = 1
-                    if j - 1 >= 0:
-                        if array[i - 1][j - 1] == 1 and (i-1, j-1) not in visited:
-                            visited.append((i-1, j-1))
-                            memo = 1
-                    if j + 1 < M:
-                        if array[i - 1][j + 1] == 1 and (i-1, j+1) not in visited:
-                            visited.append((i-1, j+1))
-                            memo = 1
-                if i+1 < N:
-                    if array[i+1][j] == 1 and (i+1, j) not in visited:
-                        visited.append((i+1, j))
-                        memo = 1
-                    if j - 1 >= 0:
-                        if array[i + 1][j - 1] == 1 and (i+1, j-1) not in visited:
-                            visited.append((i+1, j-1))
-                            memo = 1
-                    if j + 1 < M:
-                        if array[i+1][j + 1] == 1 and (i+1, j+1) not in visited:
-                            visited.append((i+1, j+1))
-                            memo = 1
-                if j - 1 >= 0:
-                    if array[i][j - 1] == 1 and (i, j - 1) not in visited:
-                        visited.append((i, j - 1))
-                        memo = 1
-                if j + 1 < M:
-                    if array[i][j + 1] == 1 and (i, j + 1) not in visited:
-                        visited.append((i, j + 1))
-                        memo = 1
-                print(visited)
-                if memo == 1:
-                    cnt += 1
+                cnt += direction(array, i, j, cnt, M, N, visited)
     return cnt
 
 
@@ -57,4 +77,4 @@ while n != 0 and m != 0:
         arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
     result = count(arr, m, n)
     print(result)
-    m, n = map(int, input().split())
+    m, n = map(int, sys.stdin.readline().rstrip().split())
